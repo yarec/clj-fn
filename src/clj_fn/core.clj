29a547ac641011557d25d33a38576b8a85314c98
yarec <yarec@163.com>
2014-11-27 18:21:58 +0800

    add load-conf file-exists

--- src/clj_fn/core.clj
+++ src/clj_fn/core.clj
@@ -20,3 +20,16 @@
 
 ;; (def m (atom {:k1 0}))
 ;; (incm m :k1)
+
+(defn file-exists [file]
+  (.exists (clojure.java.io/as-file file)))
+
+
+(def config (atom {}))
+(defn load-conf [fname]
+  (let [_fname (str fname ".mine")
+        fname (if (file-exists _fname) _fname fname)
+        conf-str (slurp fname)
+        conf-yaml (yaml/parse-string conf-str)]
+    (reset! config conf-yaml)
+    ))
